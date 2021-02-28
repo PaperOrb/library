e6dfd7249f4519cceb7d628aa5102085c114f891
PaperOrb <48017994+PaperOrb@users.noreply.github.com>
2021-02-28 03:03:50 -0500

    feat: addBookToLibrary function

--- public/js/index.js
+++ public/js/index.js
@@ -1,33 +1,42 @@
-let booksArray = [];
-let form = document.querySelector("#form");
 // let authorInput = document.querySelector('#author')
 // let titleInput = document.querySelector('#title')
 
-function Book() {
-  // creates book object
-}
+function Book(title, author, cover, pagesRead, totalPages) {
+  this.title = title;
+  this.author = author;
+  this.cover = cover;
+  this.pagesRead = pagesRead;
+  this.totalPages = totalPages;
+};
 
 function addBookToLibrary() {
-  // popup message of !pagesReadIsValid
-  pagesReadIsValid();
-  // calls Book(input fields) when done is clicked
-  //
-}
+  form.addEventListener("submit", function (event) {
+    let form = document.querySelector("#form");
+    let title = form[0].value;
+    let author = form[1].value;
+    let cover = form[2].value;
+    let pagesRead = document.querySelector('#pages-read').value;
+    let totalPages = document.querySelector("#total-pages").value;
+    let errorDiv = document.querySelector('#error');
+    let book = new Book(title, author, cover, pagesRead, pagesRead, totalPages);
 
-function pagesReadIsValid() {
-  form.addEventListener("submit", function (e) {
-    let pagesRead = document.querySelector('#pages-read');
-    let totalPages = document.querySelector("#total-pages");
-    let errorDiv = document.querySelector('#error')
-
-    if (pagesRead.value > totalPages.value) {
-      errorDiv.classList.remove('hidden')
-      e.preventDefault();
+    if (Number(pagesRead) > Number(totalPages)) {
+      errorDiv.classList.remove('hidden');
+      event.preventDefault();
     } else {
-      errorDiv.classList.add('hidden')
+      errorDiv.classList.add('hidden');
     };
+
+    let strBooksArray = ''
+    let booksArray = []
+    
+    booksArray = strBooksArray === undefined ? [] : JSON.parse(strBooksArray);
+    strBooksArray = JSON.stringify(localStorage.books);
+
+    booksArray.push(book);
+    localStorage.setItem('books', strBooksArray);
   });
-}
+};
 
 function setCoverImage() {
   // downloads image
